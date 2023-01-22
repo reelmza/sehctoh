08c364e484b37d57bb4c379f4836af37d24339c3
Jesse Moses <jessemoses71@gmail.com>
2023-01-22 15:22:36 +0100

    Form modifications.

--- src/routes/Footer.js
+++ src/routes/Footer.js
@@ -25,8 +25,6 @@ const Footer = () => {
   return (
     <div className="w-full flex flex-wrap justify-between px-5 lg:px-32 py-10 bg-slate-900">
       <form
-        method="POST"
-        action="/contact"
         className="form w-full mb-10 lg:mb-0 lg:w-[25%]"
         name="contact"
         data-netlify="true"
